b0377dd6b6c7e20246ed84f0e48cfc048a57cef7
Amirhossein Naseri <theahnaseri@gmail.com>
2023-05-16 19:59:37 +0330

    add stack class for project data structure

---
+++ src/stack.py
@@ -0,0 +1,14 @@
+# Stack Data Structure Implementation
+'''Stack class that have push, pop and size methods'''
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
